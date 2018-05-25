fc81fa8c37b23d5e3ed1d3d2cf116c5052b93d63
Marcel Eckert <marcel_eckert@hotmail.de>
2018-05-25 13:10:21 +0200

    First version for Liferay 7

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,33 @@
+AUI().ready(
+	'liferay-sign-in-modal',
+	function(A) {
+		var signIn = A.one('.sign-in > a');
+
+		if (signIn && signIn.getData('redirect') !== 'true') {
+			signIn.plug(Liferay.SignInModal);
+		}
+	}
+);
+
+function moveScroller() {
+    var $anchor = $("#scroller-anchor");
+    var $scroller = $('#scroller');
+
+    var move = function() {
+        var st = $(window).scrollTop();
+        var ot = $anchor.offset().top;
+        if(st > ot) {
+            $scroller.css({
+                position: "fixed",
+                top: "0px"
+            });
+        } else {
+            $scroller.css({
+                position: "relative",
+                top: ""
+            });
+        }
+    };
+    $(window).scroll(move);
+    move();
+}
